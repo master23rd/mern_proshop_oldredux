51ad083fb94e83de9861b6368882d1b5fbe7aad7
Adam A Prasetya <adamworks23@gmail.com>
2023-04-29 17:52:50 +0700

    homescreen listing, rating, router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,42 @@
 // import { useState } from 'react'
-// import reactLogo from './assets/react.svg'
-// import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { Container } from 'react-bootstrap'
+// import './old/App.css'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
-function App() {
-  // const [count, setCount] = useState(0)
-
+//function App() {
+const App = () => {
   return (
-    <>
-      <h1>Welcome to proshop</h1>
-    </>
+    // <>
+    //   <Header />
+    //   <main className='py-3'>
+    //     <Container>
+    //       <HomeScreen />
+    //     </Container>
+    //   </main>
+    //   <Footer />
+    // </>
+
+    <Router>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          {/* route v.6 */}
+          <Routes>
+            {/* route v.5 */}
+            {/* <Route path='/' Component={HomeScreen} /> */}
+
+            {/* route v.6 */}
+            <Route path='/' element={<HomeScreen />} exact />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
+    </Router>
   )
 }
 
---
+++ frontend/src/components/Product.jsx
@@ -0,0 +1,39 @@
+/* eslint-disable react/prop-types */
+import React from 'react'
+import { Card } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import Rating from './Rating'
+
+const Product = (props) => {
+  return (
+    <Card className='my-3 p-3 rounded'>
+      <a href={`/product/${props.product._id}`}>
+        <Card.Img src={props.product.image} variant='top' />
+      </a>
+      <Card.Body>
+        <a href={`/product/${props.product._id}`}>
+          <Card.Title as='div'>
+            <strong>{props.product.name}</strong>
+          </Card.Title>
+        </a>
+        {/* <Card.Text as='div'>
+          <div className='my-3'>
+            {props.product.rating} from {props.product.numReviews}
+          </div>
+        </Card.Text> */}
+
+        <Card.Text as='div'>
+          <Rating
+            value={props.product.rating}
+            text={`${props.product.numReviews} reviews`}
+            // color={'red'}
+          />
+        </Card.Text>
+
+        <Card.Text as='h3'>${props.product.price}</Card.Text>
+      </Card.Body>
+    </Card>
+  )
+}
+
+export default Product
