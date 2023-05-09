e22a27bf40dbd49ccf09a67ad789640766f173d0
Adam A Prasetya <adamworks23@gmail.com>
2023-05-09 16:09:13 +0700

    add redux

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGO_URI)
+    const conn = await mongoose.connect(process.env.MONGO_URI_LOCAL)
     console.log(`mongodb connected ${conn.connection.host}`.cyan.underline)
   } catch (error) {
     console.log(`Error: ${error.message}`.red.underline.bold)
