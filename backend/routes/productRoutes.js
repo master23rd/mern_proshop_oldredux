7d04f65e54052d06f0a2f9eda5c5b0096f1e59eb
Adam A Prasetya <adamworks23@gmail.com>
2023-10-27 19:56:45 +0700

    add product service

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -2,16 +2,18 @@ import express from 'express'
 const router = express.Router()
 
 import {
+  createProduct,
   deleteProduct,
   getProductById,
   getProducts,
+  updateProduct,
 } from '../controller/productController.js'
 import { protect, admin } from '../middleware/authMiddleware.js'
 
 // @desc    Fetch all products
 // @route   GET /api/products
 // @access  Public
-router.route('/').get(getProducts)
+router.route('/').get(getProducts).post(protect, admin, createProduct)
 
 //direct route
 //router.get('/', getProducts)
@@ -20,7 +22,11 @@ router.route('/').get(getProducts)
 // @route   GET /api/products/:id
 // @access  public
 
-router.route('/:id').get(getProductById).delete(protect, admin, deleteProduct)
+router
+  .route('/:id')
+  .get(getProductById)
+  .delete(protect, admin, deleteProduct)
+  .put(protect, admin, updateProduct)
 
 //direct route
 //router.get('/:id', getProductById)
