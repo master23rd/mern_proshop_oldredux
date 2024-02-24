93a338286dff117cc58fa37f2eb6c9239833b66d
Adam A Prasetya <adamworks23@gmail.com>
2024-02-24 22:34:36 +0700

    update upload routes

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
@@ -7,6 +8,7 @@ import connectDB from './config/db.js'
 import productRoutes from './routes/productRoutes.js'
 import userRoutes from './routes/userRoutes.js'
 import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -23,6 +25,7 @@ app.get('/test', (req, res) => {
 app.use('/api/products', productRoutes)
 app.use('/api/users', userRoutes)
 app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
 app.use('/api/config/paypal', (req, res) => {
   res.send(process.env.PAYPAL_CLIENT_ID)
 })
@@ -48,6 +51,12 @@ app.use('/api/config/paypal', (req, res) => {
 //   res.status(404)
 //   next(error)
 // })
+
+const __dirname = path.resolve()
+
+//dirname only work on es modules not common js
+//use local storage for image
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 app.use(notFound)
 app.use(errorHandler)
